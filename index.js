8c5fe2225b0206d8ebc468b2d26b6cbb409cc573
Dustin Whisman <whisman.dustin@gmail.com>
2022-12-11 09:50:31 -0700

    feat: set up super simple websocket server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+console.log('websocket server set up on port 8080');
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
